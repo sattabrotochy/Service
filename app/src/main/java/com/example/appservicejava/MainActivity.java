53889723a236e13f0bbf3cebca57b9ac34fffd35
chosotto09@gmail.com <chosotto09@gmail.com>
2023-05-24 23:32:11 +0600

    alarm manager

--- app/src/main/java/com/example/appservicejava/MainActivity.java
+++ app/src/main/java/com/example/appservicejava/MainActivity.java
@@ -2,16 +2,23 @@ package com.example.appservicejava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
 
 
-    Button start,stop;
+    Button start,stop,button3;
+    EditText editText;
+    AlarmManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,26 @@ public class MainActivity extends AppCompatActivity {
 
         start=findViewById(R.id.button);
         stop=findViewById(R.id.button2);
+        button3=findViewById(R.id.button3);
+        editText=findViewById(R.id.editTextTextPersonName);
+
+        manager= (AlarmManager) getSystemService(ALARM_SERVICE);
+
+
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int time=Integer.parseInt(editText.getText().toString().trim());
+
+                long tiggertime= System.currentTimeMillis()+(time* 1000L);
+                Intent intent=new Intent(MainActivity.this, MyReceiver.class);
+                PendingIntent pendingIntent=PendingIntent.getBroadcast(MainActivity.this,101,intent,0);
+                manager.set(AlarmManager.RTC_WAKEUP,tiggertime,pendingIntent);
+            }
+        });
+
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/appservicejava/MyReceiver.java
@@ -0,0 +1,19 @@
+package com.example.appservicejava;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.provider.Settings;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        mediaPlayer= MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+    }
+}
