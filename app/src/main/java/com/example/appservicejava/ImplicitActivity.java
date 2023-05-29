94368d67e175ca2df2d01cd0ac897cbf0c8cbc55
chosotto09@gmail.com <chosotto09@gmail.com>
2023-05-29 23:10:19 +0600

    implicit in java

--- app/src/main/java/com/example/appservicejava/ImplicitActivity.java
+++ app/src/main/java/com/example/appservicejava/ImplicitActivity.java
@@ -33,6 +33,13 @@ public class ImplicitActivity extends AppCompatActivity {
         });
 
 
+
+
+
+
+
+
+
         btnMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
