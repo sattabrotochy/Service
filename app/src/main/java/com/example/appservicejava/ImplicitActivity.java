6c564374c9e3122900de38b5eecca79b87d2349d
chosotto09@gmail.com <chosotto09@gmail.com>
2023-05-30 23:28:27 +0600

    broad cast receiver

--- app/src/main/java/com/example/appservicejava/ImplicitActivity.java
+++ app/src/main/java/com/example/appservicejava/ImplicitActivity.java
@@ -49,4 +49,51 @@ public class ImplicitActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    // brodcast recever app
+    // send datat
+
+
+    //   public void sendData(View view) {
+    //        Intent intent=new Intent();
+    //        intent.setAction("com.satta.broadCast");
+    //        intent.setFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+    //        sendBroadcast(intent);
+    //    }
+
+
+    // manefast file data
+
+//     <receiver  2nd app meni
+//    android:name=".MyBroadCastReceiver"
+//    android:enabled="true"
+//    android:exported="true">
+//            <intent-filter>
+//                <action android:name="com.satta.broadCast"/>
+//            </intent-filter>
+//        </receiver>
+
+
+    // receiver main activity 2nd app
+    // IntentFilter intentFilter=new IntentFilter("com.satta.broadCast");
+    //        MyBroadCastReceiver myBroadCastReceiver=new MyBroadCastReceiver();
+    //        registerReceiver(myBroadCastReceiver,intentFilter);
+
+
+    // broad cast receiver class
+
+    //public class MyBroadCastReceiver extends BroadcastReceiver {
+    //
+    //    @Override
+    //    public void onReceive(Context context, Intent intent) {
+    //        // TODO: This method is called when the BroadcastReceiver is receiving
+    //        // an Intent broadcast.
+    //
+    //        Toast.makeText(context, "lallallallalallalla", Toast.LENGTH_LONG).show();
+    //    }
+    //}
+
+
+
 }
